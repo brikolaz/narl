600fc9a4acfd934fabb9dc4fffb35e1f71d24d48
Bartosz Konikiewicz <izdwuut@gmail.com>
2026-08-07 18:34:05 +0200

    Remove multiple EQ slots from items

--- src/game/model/entities/items/helmet/HelmetEntityManual.ts
+++ src/game/model/entities/items/helmet/HelmetEntityManual.ts
@@ -1,6 +1,8 @@
 import type { Entity } from "../../../../../core/model/Entity";
 import { upsertComponents } from "../../../../../core/model/queries/components/add";
 import { hasComponentsByType } from "../../../../../core/model/queries/components/has";
+import { removeComponentsByType } from "../../../../../core/model/queries/components/remove";
+import { HeadComponent } from "../../../components/eq/HeadComponent";
 import { PantsComponent } from "../../../components/eq/PantsComponent";
 import { SpikeComponent } from "../../../components/items/SpikeComponent";
 import type { Manual } from "../../../Manual";
@@ -8,14 +10,12 @@ import { getInspectedTimes } from "../../../queries/inspect";
 
 export const HelmetEntityManual: Manual = {
   curse(_gameAction, item) {
+    removeComponentsByType(item, HeadComponent);
     upsertComponents(item, PantsComponent());
   },
 
   shouldBeCursed(item: Entity): boolean {
     const inspected = getInspectedTimes(item);
-    return (
-      hasComponentsByType(item, SpikeComponent) &&
-      inspected >= 10
-    );
+    return hasComponentsByType(item, SpikeComponent) && inspected >= 10;
   },
 };
