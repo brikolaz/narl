d3636f50c7224276a569b923e1e972bff2a3dee5
Bartosz Konikiewicz <izdwuut@gmail.com>
2026-08-14 23:14:33 +0200

    Add positions to initial items

--- src/game/systems/init/initWorld.ts
+++ src/game/systems/init/initWorld.ts
@@ -1,7 +1,5 @@
 import { upsertComponents } from "../../../core/model/queries/components/add";
-import {
-  patchComponentByType
-} from "../../../core/model/queries/components/patch";
+import { patchComponentByType } from "../../../core/model/queries/components/patch";
 import { MAP_SIZE } from "../../../utils/constants";
 import { getDummyArray } from "../../../utils/getDummyArray";
 import { PositionComponent } from "../../model/components/PositionComponent";
@@ -12,6 +10,7 @@ import { RingEntityFactory } from "../../model/entities/items/ring/RingEntity";
 import { SwordEntityFactory } from "../../model/entities/items/SwordEntity";
 import { RageBaitEntityFactory } from "../../model/entities/mobs/rageBait/RageBaitEntity";
 import type { WorldState } from "../../state/state";
+import { setPosition } from "../position/position";
 
 export const initWorld = (): WorldState => {
   const world: WorldState = getDummyArray(MAP_SIZE).map((_, position) => ({
@@ -21,28 +20,28 @@ export const initWorld = (): WorldState => {
     mobs: [],
     position,
   }));
-  world[3].items.push(RingEntityFactory.getDefault());
-  world[3].items.push(SwordEntityFactory.getDefault());
-  world[4].items.push(HelmetEntityFactory.getHornedHelmet());
-  upsertComponents(world[0].floor, VisitedComponent());
+
+  const ring = RingEntityFactory.getDefault();
+  world[3].items.push(ring);
+  setPosition(ring, 3);
+
+  const sword = SwordEntityFactory.getDefault();
+  world[3].items.push(sword);
+  setPosition(sword, 3);
+
+  const hornedHelmet = HelmetEntityFactory.getHornedHelmet();
+  world[4].items.push(hornedHelmet);
+  setPosition(hornedHelmet, 4);
+
   world[5].mobs.push(RageBaitEntityFactory.getDefault());
+  setPosition(world[5].mobs[0], 5);
   world[6].mobs.push(RageBaitEntityFactory.getDefault());
+  setPosition(world[6].mobs[0], 6);
+
   world[7].mobs.push(RageBaitEntityFactory.getDefault());
-  patchComponentByType(
-    world[5].mobs[0],
-    PositionComponent,
-    (component) => (component.position = 5),
-  );
-  patchComponentByType(
-    world[6].mobs[0],
-    PositionComponent,
-    (component) => (component.position = 6),
-  );
-  patchComponentByType(
-    world[7].mobs[0],
-    PositionComponent,
-    (component) => (component.position = 7),
-  );
+  setPosition(world[7].mobs[0], 7);
+
+  upsertComponents(world[0].floor, VisitedComponent());
 
   return world;
 };
