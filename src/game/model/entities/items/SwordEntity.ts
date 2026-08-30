ddba4f074f8b867e1555106c61c74e4391a046d9
Bartosz Konikiewicz <izdwuut@gmail.com>
2026-08-30 19:05:11 +0200

    Change default Sword damage

--- src/game/model/entities/items/SwordEntity.ts
+++ src/game/model/entities/items/SwordEntity.ts
@@ -1,5 +1,6 @@
 import { getEntityCreator } from "../../../../core/model/Entity";
 import { upsertComponents } from "../../../../core/model/queries/components/add";
+import { getRng } from "../../../systems/rng/rng";
 import { GlyphComponent } from "../../components/display/GlyphComponent";
 import { NameComponent } from "../../components/display/NameComponent";
 import { MainHandComponent } from "../../components/eq/MainHandComponent";
@@ -14,7 +15,8 @@ export const SwordEntity = getEntityCreator("SWORD");
 export const SwordEntityFactory: ItemFactory = {
   getDefault: () => {
     const sword = SwordEntity();
-
+    const minDmg = getRng(sword).range(4, 6);
+    
     upsertComponents(
       sword,
       GlyphComponent({
@@ -23,7 +25,8 @@ export const SwordEntityFactory: ItemFactory = {
       NameComponent({ name: "Sword" }),
       RemovableComponent(),
       MainHandComponent(),
-      DmgComponent({ min: 5, max: 8 }),
+
+      DmgComponent({ min: minDmg, max: minDmg + 3 }),
       PickupableComponent(),
       DroppableComponent(),
     );
