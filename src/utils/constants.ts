e980796fa66fab9f19ba15d8f5b832c7570056f6
Laura Nara <33417130+Raxenite@users.noreply.github.com>
2026-09-18 17:47:58 +0200

    Change end game gate from tile 50 to 100

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -18,6 +18,6 @@ export const ITEMS_RNG_NAMESPACE = "items" as const
 
 export const MAX_VISIBLE_LOGS = 10 as const
 
-export const END_GAME_GATE_POSITION = 50 - 1
+export const END_GAME_GATE_POSITION = 100 - 1
 export const BARE_BLOCK = 1
 export const PURSUERS_BREAKPOINT = 10
