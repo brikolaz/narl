34375d5097ace105e0d981285e3a04b77160dd86
Laura Nara <33417130+Raxenite@users.noreply.github.com>
2026-09-18 12:48:40 +0200

    Remove bailout in runWorldTurn after consuming turn

--- src/game/systems/world/turn/runWorldTurn.ts
+++ src/game/systems/world/turn/runWorldTurn.ts
@@ -29,9 +29,6 @@ export const runWorldTurn = (context: DrainContext): DrainedResolution => {
     const worldResult = drainAction(worldAction, context)
 
     consumesTurn ||= worldResult.consumesTurn
-    if (consumesTurn) {
-      break
-    }
   }
 
   const dequeuedActions = dequeueTimedActions([...context.processedActions])
