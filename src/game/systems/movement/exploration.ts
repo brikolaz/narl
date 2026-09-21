25bd880ed0214f702d509c5d70080796c4668e47
Laura Nara <33417130+Raxenite@users.noreply.github.com>
2026-09-21 12:44:07 +0200

    Don't stack Visited components

--- src/game/systems/movement/exploration.ts
+++ src/game/systems/movement/exploration.ts
@@ -1,7 +1,13 @@
 import { upsertComponents } from "../../../core/model/queries/components/add"
+import { hasComponentsByType } from "../../../core/model/queries/components/has"
 import { VisitedComponent } from "../../model/components/state/VisitedComponent"
 import { getTile } from "../world/tile"
 
 export const markAsVisited = (position: number): void => {
-  upsertComponents(getTile(position).floor, VisitedComponent())
+  const floor = getTile(position).floor
+  if (hasComponentsByType(floor, VisitedComponent)) {
+    return
+  }
+
+  upsertComponents(floor, VisitedComponent())
 }
